2caac171c8ed0323b3aaed3bf5e14d3e409a9d39
Riley Shaw <rlyshw@gmail.com>
2015-03-13 21:50:10 -0400

    basic webserver

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hello world!")
+var http = require('http');
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end("Hello World\n");
+}).listen(3000, '0.0.0.0');
+console.log('Server running at http://localhost:3000/');
